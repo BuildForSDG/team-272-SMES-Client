c51302bfcad4203a7d9b7b3ba198dc887012912c
deborahtrez <deborahtrez12@gmail.com>
2020-05-07 16:44:39 +0300

    Created Admin dashboard with its child components and deleted html code in app.tml

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OverviewComponent } from './dashboard/overview/overview.component';
+import { FundersComponent } from './funders/funders.component';
+import { SmesComponent } from './smes/smes.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent,
+    children: [
+      { path: 'Overview', component: OverviewComponent},
+      { path: 'Funders', component: FundersComponent},
+      { path: 'Smes', component: SmesComponent,}]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
