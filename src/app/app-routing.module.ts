349a73f026e90bcfe809449d6bff4f5986dd1f74
Denis Moini <denismoini@yahoo.com>
2020-05-09 15:14:02 +0300

    sign-in component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninAdminComponent } from './admin/signin/admin-signin.component';
+import { SigninFunderComponent } from './funder/signin/funder-signin.component';
+import { SigninSmeComponent } from './sme/signin/sme-signin.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'admin/sign-in', component: SigninAdminComponent },
+  { path: 'funder/sign-in', component: SigninFunderComponent },
+  { path: 'sme/sign-in', component: SigninSmeComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SigninAdminComponent } from './admin/signin/admin-signin.component';
+import { SigninFunderComponent } from './funder/signin/funder-signin.component';
+import { SigninSmeComponent } from './sme/signin/sme-signin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninAdminComponent,
+    SigninFunderComponent,
+    SigninSmeComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
