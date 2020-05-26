270c75b71ac3fa4941bfc39ff8e801477ccb7bb9
oxenprogrammer <emmy1000okello@gmail.com>
2020-05-26 17:38:32 +0300

    add travis

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  coverageReporters: ['html', 'text', 'text-summary']
+  coverageReporters: ['html', 'text', 'text-summary'],
+  projects: [
+    {
+      displayName: 'cypress',
+      runner: 'cypress-jest',
+      testMatch: ['<rootDir>/**/*-cy.js'],
+    },
+    {
+      displayName: 'test' // Your other unit Tests with jest
+    }
+  ]
 };
