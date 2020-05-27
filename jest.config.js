586b17943484a962e6c87de6fa5569e0649248a8
oxenprogrammer <emmy1000okello@gmail.com>
2020-05-27 09:40:28 +0300

    add jest for unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  coverageReporters: ['html', 'text', 'text-summary'],
-  projects: [
-    {
-      displayName: 'cypress',
-      runner: 'cypress-jest',
-      testMatch: ['<rootDir>/**/*-cy.js'],
-    },
-    {
-      displayName: 'test' // Your other unit Tests with jest
-    }
-  ]
+  coverageReporters: ['html', 'text', 'text-summary']
 };
