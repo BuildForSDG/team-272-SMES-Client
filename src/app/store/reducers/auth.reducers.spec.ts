53bea23c0d539fdccea9fc9dd694b8f7c8a71e42
oxenprogrammer <emmy1000okello@gmail.com>
2020-05-28 18:26:55 +0300

    test initial auth state

--- src/app/store/reducers/auth.reducers.spec.ts
+++ src/app/store/reducers/auth.reducers.spec.ts
@@ -1,57 +1,16 @@
-import { SignInComponent } from './../../components/sign-in/sign-in.component';
-import { AuthService } from './../../services/auth.service';
-import { AuthActionTypes, LogInSuccess, All } from './../actions/auth.actions';
-import { reducer } from './auth.reducers';
-import { TestBed, async } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import * as fromReducers from './auth.reducers';
+import * as fromActions from './../actions/auth.actions';
+import { User } from './../../components/user/user.model';
 
-
-describe('Reducer: AuthReducer', () => {
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        { provide: HttpClient, useValue: null },
-        {
-          provide: AuthService,
-          useValue: {
-            getAttendees: () => {}
-          }
-        }
-      ],
-      declarations: [SignInComponent],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  }));
-
-  it('should have initial state', () => {
-    const expected = {
-      isAuthenticated: false,
-      user: null,
-      errorMessage: null
-    };
-    const action = { type: 'foo' } as any;
-    expect(reducer(undefined, action)).toEqual(expected);
-  });
-
-  it('should have successfully login a user', () => {
-    const state = {
-      isAuthenticated: false,
-      user: null,
-      errorMessage: null
-    };
-    const action = { type: AuthActionTypes.LOGIN_SUCCESS};
-    const expected = {
-      ...state,
-        isAuthenticated: true,
-        user: {
-          token: '1234567',
-          email: 'test@gmail.com'
-        },
-        errorMessage: null
-     };
-    expect(reducer(state, action)).toEqual(expected);
-  });
+describe('Reducer: Auth Reducer', () => {
+    describe('Default State', () => {
+        it('Should return the default state', () => {
+            const { initialUserState } = fromReducers;
+            const action = {};
+            const state = fromReducers.reducer(undefined, action);
+            expect(state).toBe(initialUserState);
+        });
+    });
 });
+
