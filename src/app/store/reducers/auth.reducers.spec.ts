6fb05a8c06ceb5826015382366a7532360a56e07
oxenprogrammer <emmy1000okello@gmail.com>
2020-05-28 21:18:57 +0300

    test LOGOUT && LOGIN_FAILURE

--- src/app/store/reducers/auth.reducers.spec.ts
+++ src/app/store/reducers/auth.reducers.spec.ts
@@ -4,29 +4,56 @@ import * as fromActions from './../actions/auth.actions';
 import { User } from './../../components/user/user.model';
 
 describe('Reducer: Auth Reducer', () => {
-    describe('Default State', () => {
-        it('Should return the default state', () => {
-            const { initialUserState } = fromReducers;
-            const action = {};
-            const state = fromReducers.reducer(undefined, action);
-            expect(state).toBe(initialUserState);
-        });
+  describe('Default State', () => {
+    it('Should return the default state', () => {
+      const { initialUserState } = fromReducers;
+      const action = {};
+      const state = fromReducers.reducer(undefined, action);
+      expect(state).toBe(initialUserState);
     });
+  });
 
-    describe('LOGIN_SUCCESS', () => {
-        it('Should login user successfully', () => {
-            const { initialUserState } = fromReducers;
-            const payload = {
-                token: '1234567',
-                email: 'test@test.com',
-            };
-            const action = new fromActions.LogInSuccess(payload);
-            const state = fromReducers.reducer(initialUserState, action);
+  describe('LOGIN_SUCCESS', () => {
+    it('Should login user successfully', () => {
+      const { initialUserState } = fromReducers;
+      const payload = {
+        token: '1234567',
+        email: 'test@test.com',
+      };
+      const action = new fromActions.LogInSuccess(payload);
+      const state = fromReducers.reducer(initialUserState, action);
 
-            expect(state.isAuthenticated).toEqual(true);
-            expect(state.user.email).toEqual('test@test.com');
-            expect(state.user.token).toEqual('1234567');
-        });
+      expect(state.isAuthenticated).toEqual(true);
+      expect(state.user.email).toEqual('test@test.com');
+      expect(state.user.token).toEqual('1234567');
+      expect(state.errorMessage).toBe(null);
     });
+  });
+
+  describe('LOGIN_FAILURE', () => {
+    it('Should fail to login user', () => {
+      const { initialUserState } = fromReducers;
+      const payload = {
+        token: '1234567',
+        email: '',
+      };
+      const action = new fromActions.LogInFailure(payload);
+      const state = fromReducers.reducer(initialUserState, action);
+
+      expect(state.isAuthenticated).toEqual(false);
+      expect(state.errorMessage).toBe('Incorrect email and/or password.');
+    });
+  });
+
+  describe('LOGOUT', () => {
+    it('Should logout a user', () => {
+      const { initialUserState } = fromReducers;
+      const action = new fromActions.Logout();
+      const state = fromReducers.reducer(initialUserState, action);
+
+      expect(state.isAuthenticated).toEqual(false);
+      expect(state).toEqual(initialUserState);
+    });
+  });
 });
 
